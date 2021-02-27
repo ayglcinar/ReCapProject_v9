a69a6362a5c3d16478b821e3cc0de54224489e55
ayglcinar <60033267+ayglcinar@users.noreply.github.com>
2021-02-27 16:21:09 +0300

    Add files via upload

---
+++ Core/CrossCuttingConcerns/Validation/ValidationTool.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Core.CrossCuttingConcerns.Validation
+{
+    public static class ValidationTool
+    {
+        public static void Validate(IValidator validator, object entity)
+        {
+            var context = new ValidationContext<object>(entity);
+            var result = validator.Validate(context);
+            // ! degilse demek
+            if (!result.IsValid)
+            {
+                throw new ValidationException(result.Errors);
+            }
+        }
+    }
+
+}
